0b4a69d288abceb6d4af8c4c3bd7edbd74b5adf8
Habib Musah <musahhabib004@gmail.com>
2025-08-30 00:31:51 +0000

    Register Book model in admin with custom display

--- Introduction_to_Django/LibraryProject/manage.py
+++ Introduction_to_Django/LibraryProject/manage.py
@@ -1,42 +1,16 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django..."
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     main()
-    
-    
-    from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
-    
-    book = 'Book'.objects.create(title="1984", author="George Orwell", publication_year=1949)
-'book'
-
-Book.objects.all()
-
-Book.title = "Nineteen Eighty-Four"
-Book.save()
-Book
