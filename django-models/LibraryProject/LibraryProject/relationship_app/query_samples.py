1aa35cd27d831397467aff02fd5f472d8999535c
Habib Musah <musahhabib004@gmail.com>
2025-09-06 14:03:19 +0000

    Updated query_samples.py to use Library.objects.get(name=library_name)

--- django-models/LibraryProject/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/LibraryProject/relationship_app/query_samples.py
@@ -7,7 +7,7 @@ def books_by_author(author_name):
 
 # List all books in a library
 def books_in_library(library_name):
-    library = Library.objects.get(name=library_name)   # ✅ required
+    library = Library.objects.get(name=library_name)  # ✅ required
     return library.books.all()
 
 # Retrieve the librarian for a library
