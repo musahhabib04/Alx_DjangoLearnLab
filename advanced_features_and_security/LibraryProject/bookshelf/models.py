666865c9d11c0192e25ec97a4ef5d754b20d0f78
Habib Musah <musahhabib004@gmail.com>
2025-09-13 21:51:52 +0000

    Add CustomUser model and manager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,27 +1,41 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        """
+        Create and return a regular user with email and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_active", True)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        """
+        Create and return a superuser with all permissions.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
+
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
 
+    objects = CustomUserManager()
+
     def __str__(self):
         return self.username
-
-class Shelf(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=200, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class ShelfBook(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE, related_name="books")
-
-    def __str__(self):
-        return self.title
-
-# Create your models here.
