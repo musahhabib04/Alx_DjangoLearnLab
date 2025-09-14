2ebe2f5980a98d0b486d29c86a7d971b4e39b6bd
Habib Musah <musahhabib004@gmail.com>
2025-09-14 11:37:19 +0000

    Add Book model with custom permissions can_create and can_delete

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -39,3 +39,38 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+# LibraryProject/relationship_app/models.py
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+    
+    
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
