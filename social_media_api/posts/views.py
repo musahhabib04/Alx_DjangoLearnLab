db746e891ca79c2983ba075e09aaf13232a334eb
Habib Musah <musahhabib004@gmail.com>
2025-10-09 18:22:48 +0000

    Fix ImportError by defining PostSerializer and CommentSerializer in posts/serializers.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,45 +1,21 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions, filters
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
-from .serializers import PostListSerializer, PostDetailSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
+from .serializers import PostSerializer, CommentSerializer
+
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').prefetch_related('comments')
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['author__username']           # filter by author username
-    search_fields = ['title', 'content']             # search by title or content
-    ordering_fields = ['created_at', 'updated_at']
-
-    def get_serializer_class(self):
-        if self.action in ['list']:
-            return PostListSerializer
-        return PostDetailSerializer
+    queryset = Post.objects.all() 
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    # Optional: route for recent posts, example extra action
-    @action(detail=False, methods=['get'])
-    def recent(self, request):
-        recent_posts = self.get_queryset()[:10]
-        serializer = self.get_serializer(recent_posts, many=True)
-        return Response(serializer.data)
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related('author', 'post')
+    queryset = Comment.objects.all() 
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['post', 'author__username']
-    search_fields = ['content']
-    ordering_fields = ['created_at', 'updated_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-# Create your views here.
