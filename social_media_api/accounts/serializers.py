0dabbc41b73fe80db493d10c8eb81fd091c51d4d
Habib Musah <musahhabib004@gmail.com>
2025-10-09 11:33:34 +0000

    Fix: add token creation and password validation in UserSerializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
@@ -21,9 +22,13 @@ class UserSerializer(serializers.ModelSerializer):
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
-            password=validated_data['password'],
+            password=validated_data['password']
         )
         user.bio = validated_data.get('bio', '')
         user.profile_picture = validated_data.get('profile_picture', None)
         user.save()
+
+        #Create a token for the new user
+        Token.objects.create(user=user)
+
         return user
