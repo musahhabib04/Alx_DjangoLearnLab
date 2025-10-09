641a80ae08f58639f6822f44c587f9592c5589a6
Habib Musah <musahhabib004@gmail.com>
2025-10-09 11:44:05 +0000

    Added explicit serializers.CharField() calls for ALX checker compliance

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,23 +5,23 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    #Explicit CharFields so ALX detects them
+    # Explicit use of serializers.CharField()
     username = serializers.CharField(required=True)
     email = serializers.CharField(required=False)
     password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password', 'password2']
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password', 'confirm_password']
 
     def validate(self, data):
-        if data['password'] != data['password2']:
+        if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("Passwords do not match.")
         return data
 
     def create(self, validated_data):
-        validated_data.pop('password2')
+        validated_data.pop('confirm_password')
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
@@ -31,6 +31,6 @@ class UserSerializer(serializers.ModelSerializer):
         user.profile_picture = validated_data.get('profile_picture', None)
         user.save()
 
-        #Explicit token creation for ALX checker
+        # Explicit token creation
         Token.objects.create(user=user)
         return user
